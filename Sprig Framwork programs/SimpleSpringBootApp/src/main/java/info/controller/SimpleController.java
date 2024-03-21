a7c00cd4a739375b234ee647e072fd23c3ca68fb
akash <akash300383@gmail.com>
2024-03-21 17:47:51 +0530

    done

--- Sprig Framwork programs/SimpleSpringBootApp/src/main/java/info/controller/SimpleController.java
+++ Sprig Framwork programs/SimpleSpringBootApp/src/main/java/info/controller/SimpleController.java
@@ -1,7 +1,10 @@
 package info.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SimpleController {
@@ -13,4 +16,19 @@ public class SimpleController {
 		//return "welcome";			// it open index.html page part of template folder 
 		return "index";
 	}
+	
+	@RequestMapping(value = "loginInfo",method = RequestMethod.POST)
+	public String checkLoginInfo(HttpServletRequest req) {
+		String emailid = req.getParameter("emailid");
+		String password = req.getParameter("password");
+		if(emailid.equals("akash@gmail.com") && password.equals("123")) {
+				return "success";
+		}else {
+				return "failure";
+		}
+	}
 }
+
+
+
+
