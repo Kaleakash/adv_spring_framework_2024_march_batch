acdb33884a9f25ea09818ea21135bbbc5968adac
akash <akash300383@gmail.com>
2024-03-21 14:43:01 +0530

    done

---
+++ Sprig Framwork programs/SpringMVCWithJPA/src/main/java/com/dao/ProductDao.java
@@ -0,0 +1,29 @@
+package com.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.bean.Product;
+
+@Repository
+public class ProductDao {
+
+	@Autowired
+	EntityManagerFactory emf;
+	
+	public String storeProduct(Product product) {
+		EntityManager manager = emf.createEntityManager();
+		
+		EntityTransaction tran = manager.getTransaction();
+		
+		tran.begin();
+			manager.persist(product);        // insert query 
+		tran.commit();
+	
+		return "Record inserted successfully";
+	}
+}
