93ed19ed0c1f24e225e26b9eb6be0ebcf3f87240
akash <akash300383@gmail.com>
2024-03-25 15:46:59 +0530

    done

--- Spring boot programs/springboot-with-jpa-data/springboot-with-jpa-data/src/main/java/com/controller/ProductController.java
+++ Spring boot programs/springboot-with-jpa-data/springboot-with-jpa-data/src/main/java/com/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.entity.Product;
 import com.service.ProductService;
@@ -15,6 +17,7 @@ public class ProductController {
 	@Autowired
 	ProductService productService;
 	
+	@RequestMapping(value = "/",method = RequestMethod.GET)
 	public String open(Model model, Product product) {
 		model.addAttribute("product", product);
 		return "index";
