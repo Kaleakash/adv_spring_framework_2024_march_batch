39acb07dac3db35c6191b105a8f60b725d46726a
akash <akash300383@gmail.com>
2024-03-26 18:18:01 +0530

    done

---
+++ Spring boot programs/Spring-boot-simple-rest-api/Spring-boot-simple-rest-api/src/main/java/com/bean/Login.java
@@ -0,0 +1,23 @@
+package com.bean;
+
+public class Login {
+private String emailid;
+private String password;
+public String getEmailid() {
+	return emailid;
+}
+public void setEmailid(String emailid) {
+	this.emailid = emailid;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+@Override
+public String toString() {
+	return "Login [emailid=" + emailid + ", password=" + password + "]";
+}
+
+}
--- Spring boot programs/Spring-boot-simple-rest-api/Spring-boot-simple-rest-api/src/main/java/com/controller/LoginController.java
+++ Spring boot programs/Spring-boot-simple-rest-api/Spring-boot-simple-rest-api/src/main/java/com/controller/LoginController.java
@@ -1,10 +1,14 @@
 package com.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bean.Login;
 
 @RestController
 public class LoginController {
@@ -37,11 +41,51 @@ public class LoginController {
 	//  http://localhost:8080/multiplePathParam/raj@gmail.com/123
 	
 	@RequestMapping(value = "multiplePathParam/{email}/{password}",method = RequestMethod.GET)
-	public String multiplePathParam(@PathVariable("email") String email,@PathVariable("password") String password) {
+	public String multiplePathParam(@PathVariable("email") String email,
+			@PathVariable("password") String password) {
 		if(email.equals("raj@gmail.com") && password.equals("123")) {
 			return "success using path param";
 		}else {
 			return "failure using path param";
 		}
 	}
+	
+	@RequestMapping(value = "postTest",method = RequestMethod.POST)
+	public String postMethodTest() {
+		System.out.println("post request");
+		return "Post test method";
+	}
+	// http://localhost:8080/signUp , method post 
+	// data in json format {"emailid":"akash@gmail.com", "password":"123"}
+	
+	@RequestMapping(value = "signUp",
+			method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String signUp(@RequestBody Login login) { // @RequestBody to extra json data from request body 
+		System.out.println(login);   // it call tostring method 
+		return "done";
+	}
+	
+	// http://localhost:8080/changePassword , method put
+	// data in json format {"emailid":"akash@gmail.com", "password":"123"}
+	
+	@RequestMapping(value = "changePassword",method = RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String changePassword(@RequestBody Login login) {
+		System.out.println(login);
+		System.out.println("call service layer to change the password");
+		return "password change done";
+	}
+	
+	// http://localhost:8080/deleteAccount/raj@gmail.com
+	
+	@RequestMapping(value = "deleteAccount/{emailid}",method = RequestMethod.DELETE)
+	public String deleteAccount(@PathVariable("emailid") String emailid) {
+		System.out.println("your account deleted");
+		return "You account deleted"+emailid;
+	}
+	
+	
 }
+
+
