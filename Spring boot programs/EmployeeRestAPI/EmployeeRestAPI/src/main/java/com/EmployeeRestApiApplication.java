17c99c5c17f22e7ef276ca6a98e555fae8fe706d
akash <akash300383@gmail.com>
2024-03-27 15:08:18 +0530

    done

--- Spring boot programs/EmployeeRestAPI/EmployeeRestAPI/src/main/java/com/EmployeeRestApiApplication.java
+++ Spring boot programs/EmployeeRestAPI/EmployeeRestAPI/src/main/java/com/EmployeeRestApiApplication.java
@@ -2,12 +2,17 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EntityScan(basePackages = "com.entity")
+@EnableJpaRepositories(basePackages = "com.repository")
 public class EmployeeRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeRestApiApplication.class, args);
+		System.err.println("Spring boot up on port number 8080");
 	}
 
 }
