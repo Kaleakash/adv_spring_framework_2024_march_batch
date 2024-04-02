e3b0f515fc66b81165c7973917f895c683ce5843
akash <akash300383@gmail.com>
2024-04-02 15:25:40 +0530

    done

--- Spring boot programs/paypal-micro-service/paypal-micro-service/src/main/java/com/service/PaypalService.java
+++ Spring boot programs/paypal-micro-service/paypal-micro-service/src/main/java/com/service/PaypalService.java
@@ -1,5 +1,7 @@
 package com.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +18,7 @@ public class PaypalService {
 	@Autowired
 	RestTemplate restTemplate;
 	
-	// emailid pid 1, steven@gmail.com 101 
+
 	public String createAccount(Paypal paypal) {
 		String emailid = paypal.getEmailid();
 		int accno = restTemplate.getForObject("http://localhost:9191/account/findAccNumber/"+emailid, Integer.class);
@@ -24,4 +26,22 @@ public class PaypalService {
 		paypalRepository.save(paypal);
 		return "Account Created sucessfully";
 	}
+	
+	public String findBalane(int pid) {
+		Optional<Paypal> result = paypalRepository.findById(pid);
+		if(result.isPresent()) {
+	Paypal paypal = result.get();
+		int accno = paypal.getAccno();
+		if(accno==-1) {
+			return "Your paypal account not link with any account";
+		}else {
+		String output = restTemplate.getForObject("http://localhost:9191/account/findBalance/"+accno, String.class);	
+		return output;
+		}
+		}else {
+			return "Paypal account not exists";
+		}
+
+	}
+	
 }
