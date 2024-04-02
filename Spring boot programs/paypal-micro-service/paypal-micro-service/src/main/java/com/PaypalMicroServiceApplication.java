171cd6bde87bac9e02df8becf3d750309e732e50
akash <akash300383@gmail.com>
2024-04-02 15:43:09 +0530

    done

--- Spring boot programs/paypal-micro-service/paypal-micro-service/src/main/java/com/PaypalMicroServiceApplication.java
+++ Spring boot programs/paypal-micro-service/paypal-micro-service/src/main/java/com/PaypalMicroServiceApplication.java
@@ -2,17 +2,24 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EntityScan(basePackages = "com.entity")
+@EnableJpaRepositories(basePackages = "com.repository")
+@EnableDiscoveryClient
 public class PaypalMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaypalMicroServiceApplication.class, args);
+		System.err.println("paypal micro service up!");
 	}
 
-	// reponsible to create the object of RestTemmplate but maintain by container. 
+	// responsible to create the object of RestTemmplate but maintain by container. 
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
